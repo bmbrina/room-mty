ad71855d4e87796519c464ffec87cee5e6dddf8e
Barbara Brina <bmbrina9@gmail.com>
2018-02-06 16:05:22 -0600

    Adds alom to the project

--- src/utils/firebase/firebase.js
+++ src/utils/firebase/firebase.js
@@ -1,6 +1,6 @@
 import * as firebase from 'firebase';
 
-var config = {
+let config = {
   apiKey: "AIzaSyDU58plDbu1fpRc5cFACkHiCzGct8munv0",
   authDomain: "room-mty.firebaseapp.com",
   databaseURL: "https://room-mty.firebaseio.com",
