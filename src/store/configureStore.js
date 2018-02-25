8bd2266038beaa67c2077ddbfc69ef10bbefc4ab
Barbara Brina <bmbrina9@gmail.com>
2018-02-24 18:18:14 -0600

    Persist entire store minus user

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -12,7 +12,8 @@ import rootReducer from '../reducers';
 const persistConfig = {
   key: 'root',
   storage: storage,
-  stateReconciler: autoMergeLevel2
+  stateReconciler: autoMergeLevel2,
+  blacklist: ['user']
 }
 
 const persistedReducer = persistReducer(persistConfig, rootReducer)
