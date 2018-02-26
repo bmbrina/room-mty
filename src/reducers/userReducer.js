fefcf4dcdd55e4ef7c9bc3a7f22886dac53ff2ae
Barbara Brina <bmbrina9@gmail.com>
2018-02-25 23:51:07 -0600

    Destroys user session

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -11,6 +11,8 @@ import {
 import objectAssign from 'object-assign';
 import initialState from './userReducer/initialState';
 
+const initial=  initialState.user
+
 export default function userReducer(
   state = initialState.user,
   action
@@ -29,7 +31,7 @@ export default function userReducer(
     case SET_USER_LASTNAME:
       return objectAssign({}, state, { lastname: action.lastname });
     case END_SESSION:
-      return objectAssign({}, state, initialState.user);
+      return objectAssign({}, state, initial);
     default:
       return state;
   }
