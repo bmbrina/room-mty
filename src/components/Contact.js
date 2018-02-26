9526e3303d6dfa91cb9d0b452373f13e2e8d9092
lucianaell <lucianaell@hotmail.com>
2018-02-26 14:59:56 -0600

    [Crear clase contact]

---
+++ src/components/Contact.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import bg from '../images/bg.jpg';
+import instagram from '../images/instagram.svg';
+
+// Since this component is simple and static, there's no parent container for it.
+const AboutPage = () => {
+  return (
+    <div className="contact"  style={{backgroundImage: `url("${bg}")`}}>
+      <div className="contact__content">
+        <h2 className="contact__title">Contact</h2>
+        <p>Marcela Torres +1 (956) 333 - 46 56</p>
+        <p>Marcela Torres +1 (956) 333 - 46 56</p>
+        <p>Marcela Torres +1 (956) 333 - 46 56/p>
+        <a href="https://www.instagram.com/room_mty/" target="_blank" className="about__social hide-on-s">
+          <img src={instagram}/>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default AboutPage;
