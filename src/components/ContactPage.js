82cf5485f0df0418790e0096b2bbb97932311ce8
Barbara Brina <bmbrina9@gmail.com>
2018-02-26 19:56:33 -0600

    Adds normal style and responsive style to contact page

--- src/components/ContactPage.js
+++ src/components/ContactPage.js
@@ -6,11 +6,12 @@ const ContactPage = () => {
   return (
     <div className="contact"  style={{backgroundImage: `url("${bg}")`}}>
       <div className="contact__content">
-        <h2 className="contact__title">Contact</h2>
-        <p>Marcela Torres +1 (956) 333 - 46 56</p>
-        <p>Marcela Torres +1 (956) 333 - 46 56</p>
-        <p>Marcela Torres +1 (956) 333 - 46 56</p>
-        <a href="https://www.instagram.com/room_mty/" target="_blank" className="about__social hide-on-s">
+        <h2 className="contact__title">Contact Us</h2>
+        <p className="contact__subtitle">Email</p>
+        <a className="contact__info" href="mailto:contact@room-mty.com?Subject=Room-Mty%20Contact">contact@room-mty.com</a>
+        <p className="contact__subtitle">Phone</p>
+        <p className="contact__info">+1 (956) 333 - 46 56</p>
+        <a href="https://www.instagram.com/room_mty/" target="_blank" className="contact__social">
           <img src={instagram}/>
         </a>
       </div>
