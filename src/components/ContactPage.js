f3a46b3e80bf460ae543c0968dbe742787bfe798
Barbara Brina <bmbrina9@gmail.com>
2018-02-26 20:01:02 -0600

    Adds real email address

--- src/components/ContactPage.js
+++ src/components/ContactPage.js
@@ -8,7 +8,7 @@ const ContactPage = () => {
       <div className="contact__content">
         <h2 className="contact__title">Contact Us</h2>
         <p className="contact__subtitle">Email</p>
-        <a className="contact__info" href="mailto:contact@room-mty.com?Subject=Room-Mty%20Contact">contact@room-mty.com</a>
+        <a className="contact__info" href="mailto:room.mt.mx@gmail.com?Subject=Room-Mty%20Contact">contact@room-mty.com</a>
         <p className="contact__subtitle">Phone</p>
         <p className="contact__info">+1 (956) 333 - 46 56</p>
         <a href="https://www.instagram.com/room_mty/" target="_blank" className="contact__social">
